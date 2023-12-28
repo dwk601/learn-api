c9141622ca59d04f24dad4d2bca1368c73b555d0
dwk601 <10829539@uvu.edu>
2023-12-27 22:31:35 -0700

    Add main.py and remove myapi.py

---
+++ main.py
@@ -0,0 +1,29 @@
+from langchain.document_loaders import JSONLoader
+import json
+from pprint import pprint
+
+def load_json(file_path):
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    return data
+
+def load_data(file_path):
+    data = load_json(file_path)
+
+    loader = JSONLoader(
+        file_path=file_path,
+        jq_schema='.[]',
+        text_content=False,
+    )
+
+    loaded_data = loader.load()
+
+    # Split by each index in the list of loaded data
+    for index, loaded in enumerate(loaded_data):
+        print(f'Index: {index}')
+        pprint(loaded)
+        print('\n')
+
+if __name__ == '__main__':
+    file_path = 'uvu-data-1.json'
+    load_data(file_path)
